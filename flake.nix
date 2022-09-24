9688eed7f7357558b3b01a371a0ca904121c55a3
Johannes Maier <johannes.maier@mailbox.org>
2022-09-24 17:18:09 +0200

    Use current master version of sdl2

--- flake.nix
+++ flake.nix
@@ -1,7 +1,12 @@
 {
   description = "Trying out using Lua with Haskell";
 
-  outputs = { self, nixpkgs }:
+  inputs.haskell-sdl2 = {
+    url = "github:haskell-game/sdl2";
+    flake = false;
+  };
+
+  outputs = { self, nixpkgs, ... }@inputs:
     let
       system = "x86_64-linux";
       pkgs = import nixpkgs {
@@ -12,8 +17,12 @@
               overrides =
                 final.lib.composeExtensions (old.overrides or (_: _: { }))
                 (hfinal: hprev: {
-                  # jailbreak because of bytestring 0.11
-                  sdl2 = final.haskell.lib.compose.doJailbreak hprev.sdl2;
+                  # need to use a recent version of sdl2, or we're
+                  # running into bugs
+                  sdl2 = final.haskell.lib.compose.overrideSrc {
+                    src = inputs.haskell-sdl2;
+                    version = "2.5.4.0";
+                  } hprev.sdl2;
                 });
             });
           })
