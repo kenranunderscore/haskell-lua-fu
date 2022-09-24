8b90fec9d1f2f149b2af7fcdcbf0aa764677c545
Johannes Maier <johannes.maier@mailbox.org>
2022-09-24 17:18:30 +0200

    Draw a rectangle on the screen

--- src-exe/Main.hs
+++ src-exe/Main.hs
@@ -1,10 +1,14 @@
 {-# LANGUAGE TypeApplications #-}
 {-# LANGUAGE OverloadedStrings #-}
 
+import Control.Monad (unless)
 import HsLua
 import Control.Monad (void)
 import Data.Version (makeVersion)
 import Prelude
+import qualified SDL
+import SDL (($=))
+import SDL.Vect
 
 factorial :: DocumentedFunction e
 factorial = defun "factorial"
@@ -23,3 +27,20 @@ main = run @HsLua.Exception $ do
     [ "print(' 5! =', factorial(5), type(factorial(5)))\n"
     , "print('30! =', factorial(30), type(factorial(30)))\n"
     ]
+  SDL.initialize [SDL.InitVideo]
+  w <- SDL.createWindow "sdl" SDL.defaultWindow { SDL.windowInitialSize = V2 2000 1000 }
+  r <- SDL.createRenderer w (-1) SDL.defaultRenderer 
+  SDL.showWindow w
+  SDL.clear r
+  let rect = SDL.Rectangle (P $ V2 200 300) (V2 100 100)
+  let loop = do
+        events <- SDL.pollEvents
+        let quit = elem SDL.QuitEvent $ map SDL.eventPayload events
+        SDL.rendererDrawColor r $= V4 140 100 30 1
+        SDL.drawRect r $ Just rect
+        SDL.present r
+        unless quit loop
+  loop
+  SDL.destroyRenderer r
+  SDL.destroyWindow w
+  SDL.quit
